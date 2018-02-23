feb6253f7f9ecb12bc1568b795a8db9fb402facc
Andrew Schwimmer <aschwimm@admin.touro.edu>
2018-02-22 20:47:24 -0500

    Updated ProjectileGui samples

--- src/schwimmer/physics/ProjectileGui.java
+++ src/schwimmer/physics/ProjectileGui.java
@@ -1,8 +1,11 @@
 package schwimmer.physics;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,6 +14,8 @@ import javax.swing.JTextField;
 
 public class ProjectileGui extends JFrame {
 
+	private JTextField textField;
+
 	public ProjectileGui() {
 		
 		setTitle("Projectile Viewer");
@@ -18,25 +23,32 @@ public class ProjectileGui extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
 		
-		panel.add(new JLabel("Hello World"));
-		panel.add(new JTextField("Text Field"));
+		
+		JPanel northPanel = new JPanel();
+		northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.X_AXIS));
+		northPanel.add(new JLabel("Hello World"));
+		northPanel.add(new JLabel("Goodbye World"));
+		panel.add(northPanel, BorderLayout.NORTH);
+		
+		
+		textField = new JTextField("Text Field");
+		panel.add(textField, BorderLayout.WEST);
 		
 		JButton button = new JButton("Button");
 		
-		button.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				System.out.println("Action Performed");
-			}
-			
-		});
+		button.addActionListener(this::changeTextField);
 		
-		panel.add(button);
+		panel.add(button, BorderLayout.CENTER);
+		panel.add(new JLabel("Can we take a break?"), BorderLayout.SOUTH);
 		
 		add(panel);
 		
+	}
+	
+	public void changeTextField(ActionEvent event) {
+		textField.setText("Action Performed");
 	}
 	
 	public static void main(String args[]) {
