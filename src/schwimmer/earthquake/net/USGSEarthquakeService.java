565994bc4fda0c93e35fabe63afa6ce7b4b48ccf
Andrew Schwimmer <aschwimm@admin.touro.edu>
2018-05-10 18:55:14 -0400

    UI works correctly

--- src/schwimmer/earthquake/net/USGSEarthquakeService.java
+++ src/schwimmer/earthquake/net/USGSEarthquakeService.java
@@ -9,4 +9,13 @@ public interface USGSEarthquakeService {
 	@GET("/earthquakes/feed/v1.0/summary/all_month.geojson")
 	Call<EarthquakeFeedModel> getAllMonth();
 	
+	@GET("/earthquakes/feed/v1.0/summary/all_week.geojson")
+	Call<EarthquakeFeedModel> getAllWeek();
+
+	@GET("/earthquakes/feed/v1.0/summary/all_hour.geojson")
+	Call<EarthquakeFeedModel> getAllHour();
+
+	@GET("/earthquakes/feed/v1.0/summary/all_day.geojson")
+	Call<EarthquakeFeedModel> getAllDay();
+	
 }
